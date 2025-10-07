9e46085c226f04ce49e687b77607a9891f8543f1
Arafat Patwary <farabipsk@gmail.com>
2025-10-07 15:42:27 +0600

    Action dispatch

--- UI/UI/src/App.tsx
+++ UI/UI/src/App.tsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./Redux/Features/Counter/counterSlice";
+
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count1, setCount1] = useState(0);
 
-  const increment =() =>{
-    setCount(count + 1);
-  };
-
-  const asynIncrement = () =>{
-    setTimeout(() =>{
-      setCount1(count1 + 1);
-    }, 2000);
-  }
+const dispatch = useDispatch();
+const handleIncrement = ()=>{
+  dispatch(increment());
+}
+const handleDecrement = ()=>{
+  dispatch(decrement());
+}
 
   return (
    <>
+   <div>
    <div>Redux Counter</div>
-   <div>
-    <h2>Increment</h2>
-    <button>0</button>
-   </div>
    
-   <div>
-    <h2>Decrement</h2>
-    <button>0</button>
+   
+    <button onClick={handleIncrement}>Increment</button>
+   <div>0</div>
+  
+    <button onClick={handleDecrement}>Decrement</button>
    </div>
    </>
   )
