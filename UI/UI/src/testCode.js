c8bc49a8b0692c8ba489a2929909b72c91cd6d66
Arafat Patwary <farabipsk@gmail.com>
2025-10-19 16:50:55 +0600

    Function currying implement

--- UI/UI/src/testCode.js
+++ UI/UI/src/testCode.js
@@ -37,8 +37,69 @@
 //? Mutable vs Immutable
 
 const employee = {name: 'LalMia',job: "Developer", city:"ChanPur"}
-const employee2 = employee;
-employee2.name= "KalaMia";
 
-console.log(employee);
-console.log(employee2);
+const employee2 ={
+    ...employee,
+    name: "KalaMia",
+    job: "SQL",
+};
+
+// console.log(employee === employee2);
+// console.log(employee);
+// console.log(employee2);
+
+//? function Currying
+
+
+// const add = (a,b) => a + b;
+ 
+ //* function curried 
+ const addCurried = (a) => (b)=> a  + b;
+ //console.log(addCurried(5)(5));
+
+ // Lexical scope
+ function testAdd(m){
+    return function(n){
+        return  m + n;
+    }
+ }
+
+ //console.log(testAdd(6)(6));
+
+ //const totalCost =(amount, discount) => amount - amount * discount;
+
+ //console.log(totalCost(100, 0.4));
+
+
+ //* function curried
+ const totalPrice = (amount)=> (discount)=> amount - amount *  discount;
+ //console.log(totalPrice(1000)(.3)); 
+
+ function totalCost(amount){
+    return function(discount){
+        return amount -  amount * discount;
+    }
+ }
+
+ //console.log(totalCost(100)(.5));
+
+ const totalPrice2 = (discount)=> (amount)=> amount - amount *discount;
+
+ const withDiscount = totalPrice2(0.3);
+
+ console.log(withDiscount(200));
+ console.log(withDiscount(1000));
+ console.log(withDiscount(100));
+
+ // multiple currying
+ function add2(a){
+     return function(b){
+        return function(c){
+            return function (d){
+                return a + b+ c + d;
+            }
+        }
+     }
+ }
+
+ console.log(add2(1)(1)(1)(1));
