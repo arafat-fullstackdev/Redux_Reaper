d75f2f531e9a1bef1c87796eea3f8db28f851343
Arafat Patwary <farabipsk@gmail.com>
2025-10-28 10:34:11 +0600

    Handel dark mode using shadcn

--- UI/UI/src/components/layout/Navbar.tsx
+++ UI/UI/src/components/layout/Navbar.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mood_toggle";
 
 export default function Navbar(){
     return(
-        <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+        <nav className="max-w-7xl mx-auto h-16 justify-between flex items-center gap-3 px-5">
           <div className="flex items-center">
          <h1>Logo</h1>
          <span className="font-bold ml-2">TaskOne</span>
@@ -10,6 +11,9 @@ export default function Navbar(){
           <div>
             <Link to="">Tasks</Link>
             <Link to="/user">Users</Link>
+          </div>
+          <div className="ml-auto">
+            <ModeToggle />
           </div>
         </nav>
     )
--- UI/UI/src/main.tsx
+++ UI/UI/src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './Redux/store.ts'
-import router from './Routers/Routers.tsx'
-import { RouterProvider } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.ts";
+import router from "./Routers/Routers.tsx";
+import { RouterProvider } from "react-router";
+import { ThemeProvider } from "./providers/them_providers.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-
-       <RouterProvider router={router} />
-    </Provider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
