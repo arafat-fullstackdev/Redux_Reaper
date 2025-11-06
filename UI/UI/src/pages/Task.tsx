80d600dfe528b322a924a9b7433ecaaf0c6d0ac0
Arafat Patwary <farabipsk@gmail.com>
2025-11-06 21:34:39 +0600

    Add Task Card

--- UI/UI/src/pages/Task.tsx
+++ UI/UI/src/pages/Task.tsx
@@ -1,10 +1,19 @@
+import TaskCard from '@/components/module/task/TaskCard';
 import { useSelector } from 'react-redux'
 export default function Task() {
   const tasks = useSelector((state) =>  state.todo.tasks);
   console.log(tasks);
   return (
-    <div>
+    <div className='mx-auto max-w-7xl px-5 mt-20'>
+      <div>
       <h1>This is Task.</h1>
+    </div>
+    <div className='space-y-5 mt-5'>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+      <TaskCard/>
+    </div>
     </div>
   );
 }
