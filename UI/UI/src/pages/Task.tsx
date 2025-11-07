9adf47c67d2cf4c92ef1292c72f0c5039e87a250
Arafat Patwary <farabipsk@gmail.com>
2025-11-07 15:46:54 +0600

    debug hook

--- UI/UI/src/pages/Task.tsx
+++ UI/UI/src/pages/Task.tsx
@@ -1,7 +1,10 @@
 import TaskCard from '@/components/module/task/TaskCard';
 import { useSelector } from 'react-redux'
+import { selectTasks } from '@/Redux/Features/Task/TaskSlice';
+
 export default function Task() {
-  const tasks = useSelector((state) =>  state.todo.tasks);
+  const tasks = useSelector(selectTasks);
+  
   console.log(tasks);
   return (
     <div className='mx-auto max-w-7xl px-5 mt-20'>
